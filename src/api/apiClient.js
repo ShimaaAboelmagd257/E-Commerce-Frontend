7b2049f2dad44056f41db4cbb8271260a919ad41
ShimaaAboelmagd257 <115953332+ShimaaAboelmagd257@users.noreply.github.com>
2026-06-30 07:03:08 +0300

    Initial commit

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -4,9 +4,7 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL: "https://happily-reward-sinless.ngrok-free.dev/api",
   headers: {
-    "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "true"
-  }
+    "Content-Type": "application/json"  }
 });
 apiClient.interceptors.request.use((config) => {
     console.log("REQUEST:", config.method, config.baseURL + config.url);
